b483d35a0e6d6e9e9c104b98611c8eba89b1509a
vut <vuminhthanh2305@gmail.com>
2016-04-11 15:25:08 +0000

    Implement TweetPost Class
    git-svn-id: https://svn.riouxsvn.com/rocket_rapport@11 8240df2a-5bb7-4fd5-a8cc-7177451c291c

--- model/tweetpost.cpp
+++ model/tweetpost.cpp
@@ -1,6 +1,36 @@
 #include "tweetpost.h"
 
+int TweetPost::id_cnt = 0;
+
 TweetPost::TweetPost()
 {
+    id = id_cnt;
+    id_cnt ++;
+}
 
+int TweetPost::getID(){ return id; }
+
+bool TweetPost::addComment(Comment cmt){
+    comments.push_back(cmt);
+    return true;
+}
+
+bool TweetPost::deleteComment(int cmt_id){
+    int cmt_index = findComment(cmt_id);
+    if(cmt_index != -1){
+        comments.erase(comments.begin() + cmt_index);
+        return true;
+    }else{
+        std::cout << "Comment not found!" <<std::endl;
+        return false;
+    }
+}
+
+int TweetPost::findComment(int cmt_id){
+    for(int i = 0; i< comments.size(); i++){
+        if(comments[i].getID() == cmt_id){
+            return i;
+        }
+    }
+    return -1;
 }
--- model/tweetpost.h
+++ model/tweetpost.h
@@ -1,12 +1,22 @@
 #ifndef TWEETPOST_H
 #define TWEETPOST_H
 #include "post.h"
+#include "comment.h"
+#include <vector>
 
-
-class TweetPost : public Post
+class TweetPost: public Post
 {
+private:
+    static int id_cnt;
+    int id;
+    std::vector<Comment> comments;
+    int findComment(int cmt_id);
 public:
     TweetPost();
+    int getID();
+    std::vector<Comment> getComments();
+    bool addComment(Comment cmt);
+    bool deleteComment(int id);
 };
 
 #endif // TWEETPOST_H
