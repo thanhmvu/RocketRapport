322956dc9855073259d8faa7c84ef5267532b470
vut <vuminhthanh2305@gmail.com>
2016-04-11 15:18:38 +0000

    Implement Comment Class
    git-svn-id: https://svn.riouxsvn.com/rocket_rapport@9 8240df2a-5bb7-4fd5-a8cc-7177451c291c

--- model/comment.cpp
+++ model/comment.cpp
@@ -1,6 +1,30 @@
 #include "comment.h"
 
+int Comment::id_cnt = 0;
+
 Comment::Comment()
 {
+    id = id_cnt;
+    id_cnt ++;
 
+}
+
+int Comment::getID(){ return id;}
+
+std::string Comment::getText(){
+    return text;
+}
+
+time_t Comment::getTime(){
+    return time;
+}
+
+bool Comment::setText(std::string new_text){
+    text = new_text;
+    return true;
+}
+
+bool Comment::setTime(time_t new_time){
+    time = new_time;
+    return true;
 }
--- model/comment.h
+++ model/comment.h
@@ -1,11 +1,26 @@
 #ifndef COMMENT_H
 #define COMMENT_H
+#include <string>
+#include <iostream>
+#include <ctime>
 
 
 class Comment
 {
+private:
+    static int id_cnt;
+    int id;
+    std::string text;
+    time_t time;
+
 public:
     Comment();
+    std::string getText();
+    time_t getTime();
+    int getID();
+
+    bool setText(std::string new_text);
+    bool setTime(time_t new_time);
 };
 
 #endif // COMMENT_H
