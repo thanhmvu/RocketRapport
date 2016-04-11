06a5c97cdac50ac29401e4885969de533d882bf6
vut <vuminhthanh2305@gmail.com>
2016-04-11 15:23:02 +0000

    Implement BlogPost Class
    git-svn-id: https://svn.riouxsvn.com/rocket_rapport@10 8240df2a-5bb7-4fd5-a8cc-7177451c291c

--- model/blogpost.cpp
+++ model/blogpost.cpp
@@ -1,6 +1,36 @@
 #include "blogpost.h"
 
+int BlogPost::id_cnt = 0;
+
 BlogPost::BlogPost()
 {
+    id = id_cnt;
+    id_cnt ++;
+}
 
+int BlogPost::getID(){ return id; }
+
+bool BlogPost::addComment(Comment cmt){
+    comments.push_back(cmt);
+    return true;
+}
+
+bool BlogPost::deleteComment(int cmt_id){
+    int cmt_index = findComment(cmt_id);
+    if(cmt_index != -1){
+        comments.erase(comments.begin() + cmt_index);
+        return true;
+    }else{
+        std::cout << "Comment not found!" <<std::endl;
+        return false;
+    }
+}
+
+int BlogPost::findComment(int cmt_id){
+    for(int i = 0; i< comments.size(); i++){
+        if(comments[i].getID() == cmt_id){
+            return i;
+        }
+    }
+    return -1;
 }
--- model/blogpost.h
+++ model/blogpost.h
@@ -1,12 +1,22 @@
 #ifndef BLOGPOST_H
 #define BLOGPOST_H
 #include "post.h"
+#include "comment.h"
+#include <vector>
 
-
-class BlogPost : public Post
+class BlogPost: public Post
 {
+private:
+    static int id_cnt;
+    int id;
+    std::vector<Comment> comments;
+    int findComment(int cmt_id);
 public:
     BlogPost();
+    int getID();
+    std::vector<Comment> getComments();
+    bool addComment(Comment cmt);
+    bool deleteComment(int id);
 };
 
 #endif // BLOGPOST_H
