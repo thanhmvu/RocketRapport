425ed6ed34ed2600031a04a9eab20ab54dcc4e75
ortiza <ortiza@8240df2a-5bb7-4fd5-a8cc-7177451c291c>
2016-04-12 23:44:09 +0000

    UPdated and checked for sql error.
    git-svn-id: https://svn.riouxsvn.com/rocket_rapport@19 8240df2a-5bb7-4fd5-a8cc-7177451c291c

--- model/dbmanager.cpp
+++ model/dbmanager.cpp
@@ -1,6 +1,6 @@
 #include "dbmanager.h"
 
-DbManager::DbManager(const QString &path)
+DbManager::DbManager()
 {
 
 }
--- model/dbmanager.h
+++ model/dbmanager.h
@@ -1,17 +1,16 @@
 #ifndef DBMANAGER_H
 #define DBMANAGER_H
 #include <string>
-#include <QtSql>
 
 
 
 class DbManager
 {
 public:
-    DbManager(const QString& path);
+    DbManager();
 
 private:
-    QSqlDatabase m_db;
+
 };
 
 #endif // DBMANAGER_H
