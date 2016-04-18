c91ef6506080027826875dd7d5b652f1d2cee0cd
ortiza <ortiza@8240df2a-5bb7-4fd5-a8cc-7177451c291c>
2016-04-18 18:11:53 +0000

    Added another method to the DbManager Unit test.
    git-svn-id: https://svn.riouxsvn.com/rocket_rapport@66 8240df2a-5bb7-4fd5-a8cc-7177451c291c

--- unittest/dbmanagertest.cpp
+++ unittest/dbmanagertest.cpp
@@ -21,5 +21,21 @@ TEST(DbManagerTest, testIntRetrieve){
     EXPECT_TRUE( test1->retrieveIntInfo("AccountID","accounts","FirstName","Chun Wai") == 2) ;
     EXPECT_TRUE( test1->retrieveIntInfo("GrpID","accounts","FirstName","Chun Wai") == 2);
     EXPECT_TRUE( test1->retrieveIntInfo("ScrpBkID","accounts","FirstName","Chun Wai") == 2);
+    EXPECT_TRUE( test1->retrieveIntInfo("BlogID","accounts","FirstName","Chun Wai") == 2);
+    EXPECT_TRUE( test1->retrieveIntInfo("TweetID","accounts","FirstName","Chun Wai") == 2);
+}
 
+TEST(DbManagerTest, testStringRetrieve){
+    DbManager *test1 = new DbManager("./testDb.db");
+}
+
+TEST(DbManagerTest, testRemoveAllMethod){
+    DbManager *test1 = new DbManager("./testDb.db");
+    test1->printAllRows("FirstName");
+    test1->rmAll();
+    test1->printAllRows("FirstName"); //Should show an empty set
+    delete test1;
+    qDebug() << "Deleted first DbManager";
+    DbManager *test2 = new DbManager("./testDb.db"); //Recreating the database manager and printing its contents should show an empty list
+    test2->printAllRows("FirstName");
 }
