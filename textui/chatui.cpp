2535f115724ef5682fd52817da5ac77e1ba58e46
teddickm <teddickm@8240df2a-5bb7-4fd5-a8cc-7177451c291c>
2016-04-20 19:09:47 +0000

    Finished the design of the ChatUI (textui). Need to add accounts to the screen and then add chat logic into runScreen.
    git-svn-id: https://svn.riouxsvn.com/rocket_rapport@97 8240df2a-5bb7-4fd5-a8cc-7177451c291c

--- textui/chatui.cpp
+++ textui/chatui.cpp
@@ -6,12 +6,48 @@ ChatUI::ChatUI()
 }
 
 void ChatUI::displayScreen() {
-    mvprintw(1, 1, "On Chat Screen");
+    for (int i = 0; i < this->getCols(); i++) {
+        mvprintw(0, i, "-");
+    }
+    for (int i = 0; i < this->getCols(); i++) {
+        mvprintw(2, i, "-");
+    }
+    for (int i = 3; i < this->getRows(); i++) {
+        mvprintw(i, (this->getCols()/2)-12, "|");
+    }
+    for (int i = 3; i < this->getRows(); i++) {
+        mvprintw(i, (this->getCols()/2)-11, "|");
+    }
+    for (int i = 0; i < (this->getCols()/2)-12; i++) {
+        mvprintw(this->getRows()-4, i, "-");
+    }
+    for (int i = 0; i < (this->getCols()/2)-12; i++) {
+        mvprintw(this->getRows()-1, i, "-");
+    }
+    for (int i = (this->getCols()/2)-10; i < this->getCols(); i++) {
+        mvprintw(this->getRows()-4, i, "-");
+    }
+    for (int i = (this->getCols()/2)-10; i < this->getCols(); i++) {
+        mvprintw(this->getRows()-1, i, "-");
+    }
+    mvprintw(1, (this->getCols()/2)-12, "ROCKET RAPPORT MESSAGING");
+    mvprintw(this->getRows()-3, 2, "My Profile");
+    mvprintw(this->getRows()-2, 2, "Main Menu");
+    mvprintw(this->getRows()-3, this->getCols()-14, "|");
+    mvprintw(this->getRows()-2, this->getCols()-14, "|");
+    mvprintw(this->getRows()-3, this->getCols()-12, "Press Home");
+    mvprintw(this->getRows()-2, this->getCols()-11, "to Send");
+    mvprintw(3, ((((this->getCols()/2)-12))/2)-2, "Users");
+    mvprintw(5, 6, "Backspace to Chat");
+    mvprintw(6, 5, "Home to View Profile");
+    for (int i = 8; i < this->getRows()-4; i++) {
+        move(i, 2);
+        addnstr("Test Username", ((this->getCols()/2)-12)-3);
+    }
     refresh();
 }
 
 void ChatUI::runScreen() {
-    mvprintw(2, 1, "In Run Screen");
     refresh();
     getch();
 }
