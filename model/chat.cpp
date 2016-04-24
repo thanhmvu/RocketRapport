444e553639b7282d9b9e552b9fcfd090fed8ea7d
teddickm <teddickm@8240df2a-5bb7-4fd5-a8cc-7177451c291c>
2016-04-24 01:49:49 +0000

    Finished the Chat class. Andrew now needs to add Database storing and loading.
    git-svn-id: https://svn.riouxsvn.com/rocket_rapport@153 8240df2a-5bb7-4fd5-a8cc-7177451c291c

--- model/chat.cpp
+++ model/chat.cpp
@@ -8,14 +8,42 @@ Chat::Chat()
     id_cnt++;
 }
 
+
+/**
+ * @brief Gets ID of this chat object.
+ */
 int Chat::getChatID(){
     return chatID;
 }
 
+
+/**
+ * @brief "Sends" a message to the database.
+ */
 void Chat::addMessage(Message *newMessage){
     messages.push_back(newMessage);
 }
 
+
+/**
+ * @brief Gets the vector of messages.
+ */
 std::vector<Message*> Chat::getMessages(){
     return messages;
+}
+
+
+/**
+ * @brief Gets the username of the user that this account is talking to.
+ */
+std::string Chat::getTalkingToUser() {
+    return this->talkingToUser;
+}
+
+
+/**
+ * @brief Sets the user that is being talked to.
+ */
+void Chat::setTalkingToUser(std::string name) {
+    this->talkingToUser = name;
 }
--- model/chat.h
+++ model/chat.h
@@ -9,12 +9,16 @@ class Chat
 private:
     static int id_cnt;
     int chatID;
+
+    std::string talkingToUser;
     std::vector<Message*> messages;
 public:
     Chat();
     int getChatID();
     void addMessage(Message *newMessage);
     std::vector<Message *> getMessages();
+    std::string getTalkingToUser();
+    void setTalkingToUser(std::string name);
 };
 
 #endif // CHAT_H
