24c86df1f2f1327a08bc3c68cd8d6f2c62e18294
vut <vuminhthanh2305@gmail.com>
2016-04-26 03:54:52 +0000

    Uncomment a line that was commented by mistake
    git-svn-id: https://svn.riouxsvn.com/rocket_rapport@198 8240df2a-5bb7-4fd5-a8cc-7177451c291c

--- model/dbmanager.cpp
+++ model/dbmanager.cpp
@@ -447,7 +447,7 @@ void DbManager::retrieveAllAccounts(std::map<QString, Account*> &accounts){
 bool DbManager::addBlogPost(const QVariant &blogPostID, const QVariant &blogID,
                             const QVariant &timeDate, const QVariant &text){
      QSqlQuery query;
-     //qDebug() << query.prepare("INSERT INTO blogPosts VALUES( (:BlogPostID), (:blogID), (:timeDate), (:text) )" );
+     qDebug() << query.prepare("INSERT INTO blogPosts VALUES( (:BlogPostID), (:blogID), (:timeDate), (:text) )" );
      query.bindValue(":BlogPostID",blogPostID);
      query.bindValue(":blogID", blogID);
      query.bindValue(":timeDate",timeDate);
