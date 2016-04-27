cb4a0c70465ceedcba7ba278652e48076aa426f0
teddickm <teddickm@8240df2a-5bb7-4fd5-a8cc-7177451c291c>
2016-04-27 19:00:26 +0000

    Got the chat scrolling to work. What a great day.
    git-svn-id: https://svn.riouxsvn.com/rocket_rapport@238 8240df2a-5bb7-4fd5-a8cc-7177451c291c

--- textui/chatui.cpp
+++ textui/chatui.cpp
@@ -254,6 +254,7 @@ void ChatUI::runScreen() {
                                     }
                                 }
                             }
+                            k++;
                         }
                     }
                     break;
