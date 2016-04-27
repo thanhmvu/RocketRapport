23b73663dba6f138dd41a3af9589adc8ce8f80a3
teddickm <teddickm@8240df2a-5bb7-4fd5-a8cc-7177451c291c>
2016-04-27 18:46:50 +0000

    Removed the print outs in the login function.
    git-svn-id: https://svn.riouxsvn.com/rocket_rapport@237 8240df2a-5bb7-4fd5-a8cc-7177451c291c

--- model/system.cpp
+++ model/system.cpp
@@ -63,17 +63,17 @@ bool System::login(QString username, QString password) {
     bool success = false;
 
     if (usernameExist(username)) {
-        qDebug() << "Valid username found";
+//        qDebug() << "Valid username found";
         // If valid, check password
         Account *queryAcc = accounts[username]; //Is most likely causing a bad allocation
         //std::cout << "Entered Value: " << password << std::endl;
         //std::cout << "Expected Value: " << queryAcc->getPassword() << std::endl;
-        qDebug() << "Entered Value: " << password;
-        qDebug() << "Expected Value: " << queryAcc->getPassword();
+//        qDebug() << "Entered Value: " << password;
+//        qDebug() << "Expected Value: " << queryAcc->getPassword();
 
         if (password == queryAcc->getPassword()) {
             //std::cout << std::endl << "Successful login!" << std::endl;
-            qDebug()<< "Successful login!";
+//            qDebug()<< "Successful login!";
             // Proceed to the main menu of the program.
             currentUser = queryAcc;
             this->setLoggedIn(true);
