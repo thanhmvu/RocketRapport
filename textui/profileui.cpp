95186860ccb92f5714d95a9b5c3f517cc53358ac
teddickm <teddickm@8240df2a-5bb7-4fd5-a8cc-7177451c291c>
2016-04-28 03:28:56 +0000

    Can move back to Main Menu and fully navigate the Profile screen. Just need to implement the fields and editing them.
    git-svn-id: https://svn.riouxsvn.com/rocket_rapport@251 8240df2a-5bb7-4fd5-a8cc-7177451c291c

--- textui/profileui.cpp
+++ textui/profileui.cpp
@@ -21,6 +21,7 @@ void ProfileUI::displayScreen() {
     tempsize /= 2;
     ss << "'s Profile";
     mvprintw(1, (this->getCols()/2)-tempsize, ss.str().c_str());
+    ss.str("");
     mvprintw(4, 1, "First Name:");
     mvprintw(6, 1, "Last Name:");
     mvprintw(8, 1, "Gender:");
@@ -65,6 +66,160 @@ void ProfileUI::runScreen() {
     while(this->getChangeScreens() == false) {
         getyx(stdscr, curY, curX);
         keyPress = getch();
+        switch(keyPress) {
+        case KEY_UP: // Move up options
+            switch(rowIndex) {
+            case 0:
+                curs_set(0);
+                move(mmy, mmx);
+                printw(">");
+                move(mmy, mmx);
+                rowIndex = 8;
+                break;
+            case 1:
+                move(fny, fnx);
+                rowIndex = 0;
+                break;
+            case 2:
+                move(lny, lnx);
+                rowIndex = 1;
+                break;
+            case 3:
+                move(gy, gx);
+                rowIndex = 2;
+                break;
+            case 4:
+                move(hay, hax);
+                rowIndex = 3;
+                break;
+            case 5:
+                move(mrey, mrex);
+                rowIndex = 4;
+                break;
+            case 6:
+                move(ay, ax);
+                rowIndex = 5;
+                break;
+            case 7:
+                move(pny, pnx);
+                rowIndex = 6;
+                break;
+            case 8:
+                curs_set(1);
+                move(mmy, mmx);
+                printw(" ");
+                move(ayy, ayx);
+                rowIndex = 7;
+                break;
+            }
+            break;
+        case KEY_DOWN: // Move down options
+            switch(rowIndex) {
+            case 0:
+                move(lny, lnx);
+                rowIndex = 1;
+                break;
+            case 1:
+                move(gy, gx);
+                rowIndex = 2;
+                break;
+            case 2:
+                move(hay, hax);
+                rowIndex = 3;
+                break;
+            case 3:
+                move(mrey, mrex);
+                rowIndex = 4;
+                break;
+            case 4:
+                move(ay, ax);
+                rowIndex = 5;
+                break;
+            case 5:
+                move(pny, pnx);
+                rowIndex = 6;
+                break;
+            case 6:
+                move(ayy, ayx);
+                rowIndex = 7;
+                break;
+            case 7:
+                curs_set(0);
+                move(mmy, mmx);
+                printw(">");
+                move(mmy, mmx);
+                rowIndex = 8;
+                break;
+            case 8:
+                curs_set(1);
+                move(mmy, mmx);
+                printw(" ");
+                move(fny, fnx);
+                rowIndex = 0;
+                break;
+            }
+            break;
+        case '\n':
+            move(curY, curX);
+            break;
+        case KEY_BACKSPACE:
+            switch(rowIndex) {
+            case 0:
+                if (this->getSystem()->getCurrentUser()->getUsername().toStdString() == this->getSystem()->getCurrentUser()->getProfileUsername()) {
+                    // User can edit
+
+                }
+                break;
+            case 1:
+                if (this->getSystem()->getCurrentUser()->getUsername().toStdString() == this->getSystem()->getCurrentUser()->getProfileUsername()) {
+                    // User can edit
+
+                }
+                break;
+            case 2:
+                if (this->getSystem()->getCurrentUser()->getUsername().toStdString() == this->getSystem()->getCurrentUser()->getProfileUsername()) {
+                    // User can edit
+
+                }
+                break;
+            case 3:
+                if (this->getSystem()->getCurrentUser()->getUsername().toStdString() == this->getSystem()->getCurrentUser()->getProfileUsername()) {
+                    // User can edit
+
+                }
+                break;
+            case 4:
+                if (this->getSystem()->getCurrentUser()->getUsername().toStdString() == this->getSystem()->getCurrentUser()->getProfileUsername()) {
+                    // User can edit
+
+                }
+                break;
+            case 5:
+                if (this->getSystem()->getCurrentUser()->getUsername().toStdString() == this->getSystem()->getCurrentUser()->getProfileUsername()) {
+                    // User can edit
+
+                }
+                break;
+            case 6:
+                if (this->getSystem()->getCurrentUser()->getUsername().toStdString() == this->getSystem()->getCurrentUser()->getProfileUsername()) {
+                    // User can edit
+
+                }
+                break;
+            case 7:
+                if (this->getSystem()->getCurrentUser()->getUsername().toStdString() == this->getSystem()->getCurrentUser()->getProfileUsername()) {
+                    // User can edit
+
+                }
+                break;
+            case 8:
+                this->changeScreens(true);
+                this->screenNumber = 1;
+                break;
+            }
+            break;
+            break;
+        }
     }
 
     this->getSystem()->getCurrentUser()->setProfileUsername(this->getSystem()->getCurrentUser()->getUsername().toStdString());
