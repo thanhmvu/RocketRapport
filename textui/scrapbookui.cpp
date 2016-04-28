2fbe2610e577dd16c1160c847b2fb2112bb615b8
teddickm <teddickm@8240df2a-5bb7-4fd5-a8cc-7177451c291c>
2016-04-28 22:53:15 +0000

    Implemented more of the Scrapbook functionality.
    git-svn-id: https://svn.riouxsvn.com/rocket_rapport@270 8240df2a-5bb7-4fd5-a8cc-7177451c291c

--- textui/scrapbookui.cpp
+++ textui/scrapbookui.cpp
@@ -19,7 +19,7 @@ void ScrapbookUI::displayScreen() {
  * @brief Loops through this method while the user is on this screen.
  */
 void ScrapbookUI::runScreen() {
-//    this->viewMyScrapbook("../index.html");
+    this->viewMyScrapbook("../index.html");
     mvprintw(2, 1, "In Run Screen");
     getch();
 
@@ -36,6 +36,26 @@ void ScrapbookUI::runScreen() {
 void ScrapbookUI::viewMyScrapbook(std::string path) {
     std::ofstream scrapbookHTML;
     scrapbookHTML.open(path, std::ios::out | std::ios::trunc);
-    scrapbookHTML << "";
+    scrapbookHTML   << "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">"
+                    << "<html xmlns=\"http://www.w3.org/1999/xhtml\">"
+                    << "<head>"
+                    << "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />"
+                    << "<link rel=\"stylesheet\" type=\"text/css\" href=\"anoceanofsky.css\" />"
+                    << "<title>"
+                    << this->getSystem()->getCurrentUser()->getUsername().toStdString()
+                    << "'s Scrapbook</title>"
+                    << "</head>"
+                    << "<body>"
+                    << "<div id=\"mainPicture\">"
+                    << "<div class=\"picture\">"
+                    << "<div id=\"headerTitle\">"
+                    << this->getSystem()->getCurrentUser()->getUsername().toStdString()
+                    << "'s Scrapbook</div>"
+                    << "</div>"
+                    << "</div>"
+                    << "<div class=\"contentBox\">"
+                    << "<div class=\"innerBox\">"
+                    << "<div class=\"contentTitle\">First Name</div>"
+                    << "<div class=\"contentText\"><p>";
     scrapbookHTML.close();
 }
