8733595a65edc7752376f0c9ba0c6424575f0724
teddickm <teddickm@8240df2a-5bb7-4fd5-a8cc-7177451c291c>
2016-04-29 11:13:34 +0000

    Implemented scrolling on the Blog Screen.
    git-svn-id: https://svn.riouxsvn.com/rocket_rapport@302 8240df2a-5bb7-4fd5-a8cc-7177451c291c

--- textui/blogui.cpp
+++ textui/blogui.cpp
@@ -3,6 +3,7 @@
 BlogUI::BlogUI(System* mainSystem)
 {
     this->setSystem(mainSystem);
+    this->scrollIndex = 0;
 }
 
 void BlogUI::displayScreen() {
@@ -40,6 +41,8 @@ void BlogUI::runScreen() {
     std::stringstream npss;
     int rowIndex = 0;
     int curY = 5, curX = 1;
+
+    this->printFormerPosts();
 
     curs_set(1);
     move(npY, npX);
@@ -95,17 +98,68 @@ void BlogUI::runScreen() {
             }
             break;
         case KEY_LEFT: // Scroll former posts
+            if (scrollIndex > 0) {
+                scrollIndex--;
+            }
+            this->printFormerPosts();
             break;
         case KEY_RIGHT: // Scroll former posts
+            for(const auto &acc: this->getSystem()->getAllAccounts()) {
+                if (acc.first.toStdString() == this->getSystem()->getCurrentUser()->getBlogTweetUsername()) {
+                    int temp = ((acc.second->getMyBlog()->getMyPosts().size())-1)-scrollIndex;
+                    if (temp > 0) {
+                        scrollIndex++;
+                    }
+                }
+            }
+            this->printFormerPosts();
             break;
         case KEY_HOME: // Post/Select option
+            switch(rowIndex) {
+            case 0: // User posts
+
+
+
+
+
+                break;
+            case 1:
+                move(curY, curX);
+                break;
+            case 2:
+                this->screenNumber = 1;
+                this->changeScreens(true);
+                break;
+            }
             break;
         case KEY_BACKSPACE: // Backspace when typing
+            switch(rowIndex) {
+            case 0: // Deletes a character
+
+                break;
+            case 1:
+                move(curY, curX);
+                break;
+            case 2:
+                move(curY, curX);
+                break;
+            }
             break;
         case '\n' :
             move(curY, curX);
             break;
         default: // Typing characters for a post
+            switch(rowIndex) {
+            case 0: // Types a character
+
+                break;
+            case 1:
+                move(curY, curX);
+                break;
+            case 2:
+                move(curY, curX);
+                break;
+            }
             break;
         }
     }
@@ -115,4 +169,48 @@ void BlogUI::runScreen() {
     this->changeScreens(false);
     clear();
     refresh();
+}
+
+
+/**
+ * @brief Clears the former posts then prints the new former post.
+ */
+void BlogUI::printFormerPosts() {
+    std::stringstream sstemp;
+    int q = 0;
+    const QString dateFormat = "h:m ap MMMM d yyyy";
+    for (int i = (this->getRows()/2)+1; i < this->getRows()-3; i++) {
+        for (int j = 0; j < this->getCols(); j++) {
+            mvprintw(i, j, " ");
+        }
+    }
+    for(const auto &acc: this->getSystem()->getAllAccounts()) {
+        if (acc.first.toStdString() == this->getSystem()->getCurrentUser()->getBlogTweetUsername()) {
+            for (int m = 0; m < acc.second->getMyBlog()->getMyPosts().size(); m++) {
+                if (q < 1) {
+                    QString datetime = acc.second->getMyBlog()->getMyPosts()[m+scrollIndex]->getTimePosted().toString(dateFormat);
+                    std::string text = acc.second->getMyBlog()->getMyPosts()[m+scrollIndex]->getText().toStdString();
+                    sstemp << datetime.toStdString();
+                    mvprintw((this->getRows()/2)+1, 1, sstemp.str().c_str());
+                    sstemp.str("");
+                    refresh();
+                    int x = 0;
+                    int y = 0;
+                    for (int a = 0; a < text.size(); a++) {
+                        sstemp << text[a];
+                        mvprintw(((this->getRows()/2)+2)+y, 1+x, sstemp.str().c_str());
+                        sstemp.str("");
+                        if (x == this->getCols()) {
+                            y++;
+                            x = 0;
+                        } else {
+                            x++;
+                        }
+                    }
+                    q++;
+                    refresh();
+                }
+            }
+        }
+    }
 }
--- textui/blogui.h
+++ textui/blogui.h
@@ -9,9 +9,12 @@ public:
     BlogUI(System* mainSystem);
 
     std::stringstream ss;
+    int scrollIndex;
 
     void displayScreen();
     void runScreen();
+
+    void printFormerPosts();
 };
 
 #endif // BLOGUI_H
